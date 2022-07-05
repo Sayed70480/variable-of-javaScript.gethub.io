488facf5926223779db174ae8f6eec14dbc62835
Syed sikandar Shah <syedsikandars4@gmail.com>
2022-07-05 12:21:34 +0500

    javascript
    variable of javascript

---
+++ js/tutjs.js
@@ -0,0 +1,21 @@
+/*writing way of variable Name
+firstname
+first_name
+first_name
+firstname
+firstname99
+*/
+
+var str = "hello word";
+var num = 300;
+document.write("result of var variable <br>");
+document.write(str + "<br>" + num);
+document.write("<br>");
+document.write("<hr>");
+
+const nil = 400;
+const nus = 300;
+
+let sum = nil + nus;
+document.write("result of const / let  variable <br>");
+document.write(sum);
